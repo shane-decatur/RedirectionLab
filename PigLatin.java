ad431ec1b93062b96a7c6d603d18a6aabacf5c3c
shane-decatur <sdecatur20@stuy.edu>
2021-01-02 17:58:50 -0500

    added improved pigLatin method

--- PigLatin.java
+++ PigLatin.java
@@ -9,4 +9,29 @@ public class PigLatin{
     }
   }
 
+  public static String pigLatin(String s){
+    String digraph = s.substring(0,2);
+    if (digraph.equals("bl") || digraph.equals("br")
+    || digraph.equals("ch") || digraph.equals("ck") || digraph.equals("cl") || digraph.equals("cr")
+    || digraph.equals("dr") || digraph.equals("fl") || digraph.equals("fr")
+    || digraph.equals("gh") || digraph.equals("gl") || digraph.equals("gr") || digraph.equals("ng")
+    || digraph.equals("ph") || digraph.equals("pl") || digraph.equals("pr") || digraph.equals("qu")
+    || digraph.equals("sc") || digraph.equals("sh") || digraph.equals("sk") || digraph.equals("sl")
+    || digraph.equals("sm") || digraph.equals("sn") || digraph.equals("sp") || digraph.equals("st")
+    || digraph.equals("th") || digraph.equals("tr") || digraph.equals("tw")
+    || digraph.equals("wh") || digraph.equals("wr")){
+      return s.substring(2) + digraph + "ay";
+    }
+    else{
+      return pigLatinSimple(s);
+    }
+  }
+
+  public static void main(String[] args){
+    System.out.println(pigLatin("the"));
+    System.out.println(pigLatin("check"));
+    System.out.println(pigLatin("skee"));
+    System.out.println(pigLatin("emu"));
+    System.out.println(pigLatin("grade"));
+  }
 }
